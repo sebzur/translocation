dbde235da31d6081c64ccfddecaa45f1d456994b
Marcin Kosmider <kosmo@codelab.pl>
2012-08-05 21:43:20 +0200

    Dodane hernie i crossing barier oraz szybciej w ElectricField

--- pluk/base.py
+++ pluk/base.py
@@ -2,12 +2,13 @@ import numpy
 
 
 class Polimer(object):
+    """Polimer """
     def __init__(self, reptons, link_length, dim, *args, **kwargs):
         self.dim = dim
         self.reptons = reptons
         link_number = reptons-1
         
-        self.positions = numpy.zeros((reptons,dim))
+        self.positions = numpy.zeros((reptons, dim))
         self.link_length = numpy.zeros(link_number)
         self.link_length.fill(link_length)
        
@@ -17,13 +18,18 @@ class Polimer(object):
     
     
 class Translation(object):
-    
+    """Lattice base class"""
     def get_translations(self):
         """ Returns all possible tranlations, note, that this will define
         the latice also.
 
         """
         raise NotImplementedError
+    
+    
+    def get_initial_translations(self):
+        """ sometimes some transletions can't be used to create polimer - [-1,1]"""
+        return self.get_translations()
     
     def get_trans_count(self):
         return self.get_translations().shape[0]
@@ -33,22 +39,34 @@ class Translation(object):
 
         
 class SquareLattice(Translation):
-    
-    vectors = numpy.array( [[0,1], [1,0], [0,-1], [-1,0]] )
+    """Square lattice"""
+    vectors = numpy.array( [[0, 1], [1, 0], [0, -1], [-1, 0] ] )
     
     def get_translations(self):
         return self.vectors
 
+
+class SecondNearestLattice(Translation):
+    vectors = numpy.array( [[0, 1], [1, 0], [0, -1], [-1, 0], [1, 1], [1, -1], [-1, -1], [-1, 1]  ] )
+    
+    def get_translations(self):
+        return self.vectors
+
+    def get_initial_translations(self):
+        return self.vectors[:4]
+    
     
 class Rule(object):
-    
+    """ Base class"""
     def __init__(self, polimer, lattice , *args, **kwargs):
         self.polimer = polimer
         self.lattice = lattice
+        self.rate = 1
         self.initialize(*args, **kwargs)
         
+        
 
-    def initialize(*args, **kwargs):
+    def initialize(self, *args, **kwargs):
         pass
     
     def get_rate(self, repton_id, trans_id, *args, **kwargs):
@@ -57,14 +75,14 @@ class Rule(object):
         
 
 class NoTension(Rule):
+    """Link can't be broken"""
     
-    rate = 1
-    
-    def get_rate(self,repton_id, trans_id, *args, **kwargs):
+    def get_rate(self, repton_id, trans_id, *args, **kwargs):
         t_vect = self.lattice.get_translation(trans_id)
         
         if ( repton_id ) > 0:
-            #znajdz dlugosc z lewej strony - oldeglosc 0-1 - length[0], 1-2 - length[1] ...
+            #znajdz dlugosc z lewej strony 
+            #oldeglosc 0-1 - length[0], 1-2- length[1] ...
             length = self.polimer.link_length[repton_id-1]
             if ((self.polimer.positions[repton_id-1] - (self.polimer.positions[repton_id]+t_vect)) ** 2).sum() > length:
                 return 0
@@ -77,85 +95,92 @@ class NoTension(Rule):
         return self.rate
     
 
+#To juz chyba niepotrzebne
+#class NoHernia(Rule):
+    #""" Hernias are not allowed"""
+    #def get_rate(self, repton_id, trans_id, *args, **kwargs):
+        #"""Zakladam, ze jezeli ruch ni ejst hernia to zwracam rate = 1 - multiplikatywnosc"""
+        #t_vect = self.lattice.get_translation(trans_id)
+        
+        #if repton_id == 0 or repton_id == self.polimer.reptons - 1:
+            #return 1
+        
+        ##srodkowy nie moze sie ruszyc !!!! - anihilacja
+        #if numpy.all(self.polimer.positions[repton_id -1] == self.polimer.positions[repton_id]) and numpy.all(self.polimer.positions[repton_id] == self.polimer.positions[repton_id+1]):
+            #return 0
+            
+        ##srodkowy nie moze wskoczyc miedzy dwa - kreacja
+        #if numpy.all(self.polimer.positions[repton_id - 1] == self.polimer.positions[repton_id+1]) and numpy.all( (self.polimer.positions[repton_id]+t_vect) == self.polimer.positions[repton_id+1]):
+            #return 0
+            
+        #return 1
+        
 
-class NoHernia(Rule):
-    
-    rate = 1
-    
-    def get_rate(self,repton_id, trans_id, *args, **kwargs):
+class Hernia(Rule):
+    """ Hernias are allowed"""
+    def initialize(self, *args, **kwargs):
+        self.rate = kwargs.get('hernia')
+        
+    def get_rate(self, repton_id, trans_id, *args, **kwargs):
         t_vect = self.lattice.get_translation(trans_id)
         
+        #pierwszy i ostatni nie tworzy herni - zwraca 1 - multiplikatywnosc
         if repton_id == 0 or repton_id == self.polimer.reptons - 1:
+            return 1
+        
+                
+        ##srodkowy moze sie ruszyc !!!! 
+        if numpy.all(self.polimer.positions[repton_id -1] == self.polimer.positions[repton_id]) and numpy.all(self.polimer.positions[repton_id] == self.polimer.positions[repton_id+1]):
             return self.rate
         
-        #srodkowy nie moze sie ruszyc !!!! - anihilacja
-        if numpy.all(self.polimer.positions[repton_id -1] == self.polimer.positions[repton_id]) and numpy.all(self.polimer.positions[repton_id] == self.polimer.positions[repton_id+1]):
-            return 0
-            
-        #srodkowy nie moze wskoczyc miedzy dwa - kreacja
+        
+        #srodkowy moze wskoczyc miedzy dwa 
+        
         if numpy.all(self.polimer.positions[repton_id - 1] == self.polimer.positions[repton_id+1]) and numpy.all( (self.polimer.positions[repton_id]+t_vect) == self.polimer.positions[repton_id+1]):
-            return 0
+            return self.rate
             
+        return 1
+
+class CrossingBarrier(Rule):
+    """ Hernias are allowed"""
+    def initialize(self, *args, **kwargs):
+        self.rate = kwargs.get('crossing')
+        
+    def get_rate(self, repton_id, trans_id, *args, **kwargs):
+        t_vect = self.lattice.get_translation(trans_id)
+        
+        #po pierwsze - jesli nie po skosie to nie barier crossing
+        if t_vect[0] == 0 or t_vect[1] == 0:
+            return 1
+        
+        # jesli reptony sasiedne w jednej komorce  to nie (hernie nie ma CB  musz byc dwie hernie)
+        if repton_id != 0 and repton_id != self.polimer.reptons-1:
+            if numpy.all(self.polimer.positions[repton_id -1] == self.polimer.positions[repton_id+1]):
+                return 0
+        
+        #odleglos sprawdzil wczesniej - wiec ok
         return self.rate
         
         
 class HorizontalElectricField(Rule):
-    
-    def initialize(self,*args, **kwargs):
-        self.B = numpy.exp(0.5*kwargs.get('epsilon'))
+    """ Apply horizontal electric field"""
+    def initialize(self, *args, **kwargs):
+        self.rate = numpy.exp(0.5*kwargs.get('epsilon'))
         
-    def get_rate(self,repton_id, trans_id, *args, **kwargs):
+    def get_rate(self, repton_id, trans_id, *args, **kwargs):
         t_vect = self.lattice.get_translation(trans_id)
         
-        if numpy.all(t_vect == [1,0]):
-            return self.B
-        
-        if numpy.all(t_vect == [-1,0]):
-            return 1./self.B
+        if t_vect[1] == 0:
+            if t_vect[0] == 1:
+                return self.rate
+            else:
+                return 1./self.rate
             
         return 1
 
 
-#UWAGA - jak nei a herni i jest zachowana dlugos to to juz jest reptation bo nie ma jak opuscic konturu
-#class PureReptation(Rule):
-    
-    
-    #def __init__(self, polimer, lattice, *args, **kwargs):
-        
-        #super(PureReptation,self).__init__(polimer, lattice, *args, **kwargs)
-        #self.rules = [ NoTension(polimer, lattice), NoHernia(polimer, lattice) ]
-    
-    #def get_rate(self,repton_id, trans_name, *args, **kwargs):
-        
-        #rate = 1
-        
-        ##check if no tension and no hernia
-        #for rule in self.rules:
-            #rate_tmp = rule.check_rule(repton_id, trans_name)
-            #if rate_tmp == 0:
-                #return 0
-            #else:
-                #rate *= rate
-        
-        #if rate == 0:
-            #return 0
-            
-        ##ostatni i pierwszy ida "normalnie"
-        #if repton_id == 0 or repton_id == self.polimer.reptons - 1:
-            #return rate
-        
-        
-        ##pozostale ida po konturze
-        #t_vect = self.lattice.get_translation(trans_name)
-        #new_pos = self.polimer.positions[repton_id] + t_vect
-        #if numpy.all(new_pos == self.polimer.positions[repton_id-1]) or numpy.all(new_pos == self.polimer.positions[repton_id+1]):
-            #return rate
-        
-        #return 0
-        
-
 class Dynamics(object):
-    
+    """Dynamics - base class"""
     lattice = None
     rules_classes = []
     
@@ -173,17 +198,18 @@ class Dynamics(object):
         self.initialize_rules(*args, **kwargs)
         self.initialize_polimer(*args, **kwargs)
         
-        self.motion_matrix = numpy.zeros( reptons * self.lattice.get_trans_count() )
+        self.motion_matrix = numpy.zeros(reptons * self.lattice.get_trans_count())
+        self.cumulative_prob = self.motion_matrix.cumsum()
         self.find_translations()
         
     def initialize_rules(self, *args, **kwargs):
-        for idx,rule in enumerate(self.rules_classes):
+        for idx, rule in enumerate(self.rules_classes):
             self.rules.append(rule(self.polimer, self.lattice, *args, **kwargs))
    
    
     def initialize_polimer(self, *args, **kwargs):
         
-        trans_number =  self.lattice.get_translations().shape[0]
+        trans_number =  self.lattice.get_initial_translations().shape[0]
         
         for repton_id, pos in enumerate(self.polimer.positions[1:]):
             repton_id += 1
@@ -199,17 +225,18 @@ class Dynamics(object):
     def get_lifetime(self, *args, **kwargs):
         cum_prob = self.cumulative_prob[-1]
         random = numpy.random.uniform(low=0.0000000001, high=1)
-        return -1.0*numpy.log(random)/cum_prob;
+        return -1.0*numpy.log(random)/cum_prob
     
         
     def reconfigure(self, *args, **kwargs):
-        
-        
-        rand_nr= numpy.random.uniform(0, self.cumulative_prob[-1])
+       # rand_nr = numpy.random.uniform(0, self.cumulative_prob[-1])
+        rand_nr = numpy.random.rand() * self.cumulative_prob[-1]
+       
         for idx, prob in enumerate(self.cumulative_prob):
             if rand_nr <= prob:
-                trans_id, repton_id = divmod(idx,self.polimer.reptons)
+                trans_id, repton_id = divmod(idx, self.polimer.reptons)
                 t_vect = self.lattice.get_translation(trans_id) 
+                print "poszedl ", repton_id , " w ", t_vect
                 break
                 
         self.polimer.positions[repton_id] += t_vect
@@ -219,7 +246,7 @@ class Dynamics(object):
     def update(self, repton_id):
         
         #wywal te co nie sa dozwolone z macierzy dozwolonych ruchow
-        for trans_id in range(0, self.lattice.get_trans_count()):
+        for trans_id in xrange(0, self.lattice.get_trans_count()):
             idx = self._get_coordinate(trans_id, repton_id)
             self.motion_matrix[idx] = 0
         
@@ -233,15 +260,15 @@ class Dynamics(object):
         
         #update
         if repton_id == 0:
-            tab = [0,1]
+            tab = [0, 1]
         elif repton_id == self.polimer.reptons - 1:
-            tab=[repton_id-1, repton_id]
+            tab = [repton_id-1, repton_id]
         else:   
-            tab=[repton_id-1, repton_id, repton_id+1]
+            tab = [repton_id-1, repton_id, repton_id+1]
         
-        for trans_id in range(0,self.lattice.get_trans_count()):
+        for trans_id in xrange(0, self.lattice.get_trans_count()):
             for repton in tab:
-                rate = self._get_rate(repton,trans_id)
+                rate = self._get_rate(repton, trans_id)
                 idx = self._get_coordinate(trans_id, repton)
                 self.motion_matrix[idx] = rate
         
@@ -249,8 +276,8 @@ class Dynamics(object):
     
         
     def find_translations(self):
-        for trans_id in range(0, self.lattice.get_trans_count()):
-            for repton_id in range(0, self.polimer.reptons):
+        for trans_id in xrange(0, self.lattice.get_trans_count()):
+            for repton_id in xrange(0, self.polimer.reptons):
                 rate  =  self._get_rate(repton_id, trans_id)
                 if rate  != 0:
                     idx = self._get_coordinate(trans_id, repton_id)
@@ -274,21 +301,23 @@ class Dynamics(object):
     
     
 class TestDynamics(Dynamics):
-    lattice = SquareLattice()
-    rules_classes = [NoTension, NoHernia, HorizontalElectricField]
+    """Electroforesis"""
+    lattice = SecondNearestLattice()
+    rules_classes = [NoTension, Hernia, CrossingBarrier, HorizontalElectricField]
     
     
         
 if __name__ == "__main__":
     
-    symulator = TestDynamics(reptons=150,link_length=1,dim=2,epsilon=0.1)
+    epsilon = 0.001
     
-    for i in range(101000):
-        time = symulator.get_lifetime()
-        t1 = symulator.polimer.get_cms_coord()[0]
-        symulator.reconfigure()
-        t2 = symulator.polimer.get_cms_coord()[0]
-        
+    symulator = TestDynamics(reptons=10, link_length=1, dim=2, epsilon=epsilon, hernia=0.5, crossing=0.3)
+    print symulator.polimer.positions
+    print symulator.motion_matrix.reshape(8,10)
+    symulator.reconfigure()
+    print symulator.polimer.positions
+    print symulator.motion_matrix.reshape(8,10)
+    
         
     
     

a7977db35a43b065a24b67649ede6ffda2aec0ac
Marcin Kosmider <kosmo@supernova.services.kdm.wcss.pl>
2012-08-07 09:39:47 +0200

    Poprawka w skyprcie inster

--- pluk/scripts/wcss/insert.sh
+++ pluk/scripts/wcss/insert.sh
@@ -4,7 +4,7 @@
 #PBS -l software=chain_translocation_C_2_N
 # ---PBS -software translocate_"$N"_"$s"_"$eps"_"$c"
 # i tak dalej
-cd /home/sebzur/git/translocation/pluk
+cd ~/git/translocation/pluk
 mpiexec -n 40 /usr/local/zgoraphysics/bin/python2.7 example.py -n $N -s $s -r 160 -e $eps -l 1 -d 2 >>scripts/wcss/output.err >> scripts/wcss/output.out	    
 
 
